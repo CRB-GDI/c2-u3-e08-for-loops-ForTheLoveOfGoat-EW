08ba963b5aeba3542bcd2885e5a0fefb5d1e0f69
Elijah Williams <145467298+ForTheLoveOfGoat-EW@users.noreply.github.com>
2024-03-05 09:06:24 -0500

    done

---
+++ Exercise 8/Exercise 8/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter maximum number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++) 
+            {
+                if (i % 2 == 0)
+                { 
+                Console.WriteLine(i);
+                }
+            }
+            //Console.WriteLine(number);
+
+            // asterisk
+
+            Console.WriteLine("Enter number of asterisk");
+            int asterisk = Int32.Parse(Console.ReadLine());
+
+            for (asterisk = asterisk; asterisk > 0; asterisk--)
+            {
+                                
+            Console.Write("*");
+                
+            }
+        }
+    }
+}
